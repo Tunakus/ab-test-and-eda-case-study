668ad2c2f28118a25396af5c6367e239d7099233
Tunakus <125119079+Tunakus@users.noreply.github.com>
2025-11-14 20:55:23 +0300

    Create task1d_plus1pct_purchase.sql

---
+++ task1_ab_test/task1d_plus1pct_purchase.sql
@@ -0,0 +1,16 @@
+-- Task 1d – Day 15 Sonrası %1 Satın Alma Oranı Artışı Etkisi
+
+SELECT
+    p.var,
+    ROUND(20000 *
+        (30*p.d1 + 54*p.d3 + 84*p.d7 + 72*p.d14) *
+        (p.pr + p.imp * p.ecpm / 1000.0), 2) AS total_money_to_day30_with_boost
+FROM (
+    SELECT 'A' AS var,
+           0.0405 AS pr, 2.3 AS imp, 9.8 AS ecpm,   -- +%1 purchase rate
+           0.53 AS d1, 0.27 AS d3, 0.17 AS d7, 0.06 AS d14
+    UNION ALL
+    SELECT 'B',
+           0.0315, 1.6, 10.8,
+           0.48, 0.25, 0.19, 0.09
+) AS p;
